8998839b51084fab0f39f9f72eef8780bcf7c4c1
KafetzisThomas <tomkafetzis06@gmail.com>
2026-06-08 16:11:05 +0300

    Move templates and static dirs to base

--- static/main.js
+++ static/main.js
@@ -9,17 +9,15 @@ socket.onclose = function (e) {
     console.log("WebSocket connection closed.");
 };
 
-// Handle incoming files
+// handle incoming files
 socket.onmessage = function (e) {
     let data = JSON.parse(e.data);
 
-    // Display the user's unique ID on the page
     if (data.type === 'user_id') {
         myUserId = data.user_id;
         document.getElementById('myUserId').innerHTML = myUserId;
     }
 
-    // Handle form submission (sending files)
     if (data.type === 'file_offer') {
         let agree = confirm(`User ${data.sender_id} is sending a file: ${data.file_name}. Do you want to download it?`);
         if (agree) {
@@ -30,7 +28,7 @@ socket.onmessage = function (e) {
 
 let form = document.getElementById('form');
 form.addEventListener('submit', (e) => {
-    e.preventDefault();  // Prevent it from doing a normal submission
+    e.preventDefault();
 
     let fileInput = document.getElementById('fileInput');
     let file = fileInput.files[0];
@@ -38,26 +36,26 @@ form.addEventListener('submit', (e) => {
 
     let reader = new FileReader();
     reader.onload = function (event) {
-        let fileData = event.target.result.split(',')[1];  // Extract base64 part
+        let fileData = event.target.result.split(',')[1];  // extract base64 part
 
         // FOR DEVELOPMENT ONLY !!!
         // console.log("Encoded: " + fileData.substring(0, 10));
 
-        // Send the file data to the selected user via WebSocket
         socket.send(JSON.stringify({
             'file_name': file.name,
             'file': fileData,
             'target_user_id': targetUserId
         }));
     };
-    reader.readAsDataURL(file);  // Convert to base64
+    reader.readAsDataURL(file);  // convert to base64
     form.reset();
 });
 
 function downloadFile(fileName, fileData) {
-    // Creates an invisible link, sets its href to the base64-encoded file data,
-    // and triggers a download with the specified filename. The browser automatically
-    // decodes the base64 string back into the original binary file upon download.
+    // 1. creates an invisible link
+    // 2. sets its href to the base64 encoded file data
+    // 3. triggers download with the specified filename.
+
     let link = document.createElement('a');
     link.href = 'data:application/octet-stream;base64,' + fileData;
     link.download = fileName;
